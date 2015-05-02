3435ef14251b3192554214bf2503cff8d26b5ec9
Philipp Oppermann <dev@phil-opp.com>
2015-05-02 12:52:15 +0200

    add paging setup code

---
+++ src/arch/x86_64/bootstrap/setup_paging.asm
@@ -0,0 +1,112 @@
+; we are still in 32bit-mode
+BITS 32
+
+; will be called by boot.asm
+global setup_paging
+
+; identity map first 8*2=16 Megabyte
+%define p1_tables 8
+
+; declare page table %1
+%macro create_page_table 1
+    align 0x1000
+    %1:
+    times 0x1000 db 0
+%endmacro
+
+; link entry %2 of page table %1 to page table %3
+%macro link_page_tables 3
+    mov eax, %3
+    or eax, 1       ; present bit
+    mov [%1 + %2 * 8], eax
+%endmacro
+
+
+section .text
+
+setup_paging:
+
+.link_page_tables:
+    ; link P4[0] to P3
+    link_page_tables P4, 0, P3
+    ; link P3[0] to P2
+    link_page_tables P3, 0, P2
+    ; link P2[i] to P1_i
+    %assign i 0
+    %rep p1_tables
+        link_page_tables P2, i, P1_%[i]
+    %assign i i+1
+    %endrep
+
+.fill_p1_tables:
+    %assign i 0
+    %rep p1_tables
+        mov edi, P1_%[i]
+        mov eax, 0x200000 * i   ; 2MB per P1 table
+        call fill_p1_table
+    %assign i i+1
+    %endrep
+
+
+; to catch null pointer dereferences
+.unmap_null_page:
+    mov dword [P1_0], 0
+    mov dword [P1_0 + 4], 0
+
+; map the page tables to the highest addresses for easier access
+.recursive_map:
+    mov eax, P4
+    or eax, 1
+    mov [P4 + 511*8], eax
+
+; page tables are set up, now we can enable paging
+.enable_paging:
+    ; load P4 to cr3 register (cpu uses this to access the first page table)
+    mov eax, P4
+    mov cr3, eax
+
+    ; enable PAE-flag in cr4 (Physical Address Extension)
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; enable long mode in the respective MSR (model specific register)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging in the cr0 register
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    ; return to boot.asm
+    ret
+
+
+; map the P1 table in edi to the 2MB following the address in eax
+fill_p1_table:
+    mov ecx, 512                ; loop variable: 512 page table entries
+    or eax, 0x3                 ; present + read/write flags
+.loop:
+    mov dword [edi], eax        ; map current entry to address in eax
+    mov dword [edi + 4], 0      ; higher bits of the address are 0
+    add edi, 8                  ; current entry <- next entry
+    add eax, 0x1000             ; current address <- next address (current+4kB)
+    sub ecx, 1                  ; decrement loop variable
+    jnz .loop
+.done:
+    ret
+
+
+section .data
+    ; page tables (P4=PML4, P3=PDPT, P2=PD, P1=PT)
+    create_page_table P4
+    create_page_table P3
+    create_page_table P2
+    %assign i 0
+    %rep p1_tables
+        create_page_table P1_%[i]
+    %assign i i+1
+    %endrep
