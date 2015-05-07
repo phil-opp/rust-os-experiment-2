d3679cde20a1402dcd3a075dc5775745a6277afc
Philipp Oppermann <dev@phil-opp.com>
2015-05-07 15:21:35 +0200

    use consts for subscriber status and directly import SeqCst

--- src/os/src/stream/spsc.rs
+++ src/os/src/stream/spsc.rs
@@ -1,8 +1,13 @@
 use std::boxed::into_raw;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering::SeqCst;
 use std::sync::mpsc::spsc_queue::Queue;
 use std::ptr::Unique;
 use super::{Sender, Subscriber};
+
+const IDLE: usize = 0;          // subscriber idle (sender has ownership)
+const WORKING: usize = 1;       // subscriber working
+const STREAM_CLOSED: usize = 2; // sender closed stream
 
 pub struct SpscSender<T, S> {
     subscriber: Unique<SubscriberLink<T, S>>
@@ -15,7 +20,7 @@ impl<T, S> SpscSender<T, S> where S: Subscriber<T> {
                 subscriber: Unique::new(into_raw(Box::new(
                     SubscriberLink{
                         queue: Queue::new(0),   // unlimited cache
-                        status: AtomicUsize::new(0),
+                        status: AtomicUsize::new(IDLE),
                         subscriber: subscriber,
                 }))),
             }            
@@ -35,9 +40,6 @@ impl<T, S> Sender<T> for SpscSender<T, S> where S: Subscriber<T> {
 
 struct SubscriberLink<T, S> {
     queue: Queue<T>,
-    // 0 = subscriber idle (sender has ownership)
-    // 1 = subscriber working
-    // 2 = sender closed stream
     status: AtomicUsize,
     subscriber: S,
 }
@@ -49,25 +51,26 @@ impl<T, S> SubscriberLink<T, S> where S: Subscriber<T> {
     }
 
     fn close(&self) {
-        match self.status.swap(2, Ordering::SeqCst) {
-            1 => {},    // subscriber is currently working and will call
-                        // the on_close function when done
-            0 => self.notify(), // subscriber is idle -> start it again to 
-                                //pop all remaining values and call on_close
-            _ => unreachable!()                
+        match self.status.swap(STREAM_CLOSED, SeqCst) {
+            WORKING => {},  // subscriber is currently working and will call
+                            // the on_close function when done
+            IDLE => self.notify(),  // subscriber is idle -> start it again to 
+                                    //pop all remaining values and call on_close
+            STREAM_CLOSED => panic!("closing already closed stream"),
+            _ => unreachable!()
         }
     }
 
     fn notify(&self) {
-        let old_status = self.status.compare_and_swap(0, 1, Ordering::SeqCst);
+        let old_status = self.status.compare_and_swap(IDLE, WORKING, SeqCst);
         match old_status {
-            0 | 2 => { 
-                // if ==0 : subscriber was idle and is now working. We have marked
-                // it as working (1) have thus exclusive access. We will set 
-                // the state back to idle (0) at the end.
-                // if ==2: Stream was closed and there won't be any new values. 
-                // But maybe there are still values in the queue. Pop them and
-                // then call on_close and drop self.
+            IDLE | STREAM_CLOSED => {
+                // IDLE: subscriber was IDLE and is now WORKING. We have marked
+                // it as WORKING and have thus exclusive access. We will set the
+                // state back to IDLE at the end.
+                // STREAM_CLOSED: Stream was closed and there won't be any new
+                // values. But maybe there are still values in the queue. Pop
+                // them and then call on_close and drop self.
 
                 // we have exclusive access
                 let s: &mut Self = unsafe{ &mut *(self as *const _ as *mut _) };
@@ -75,17 +78,17 @@ impl<T, S> SubscriberLink<T, S> where S: Subscriber<T> {
                 // TODO spawn task
                 {
                     unsafe{ s.pop_values() };
-                    // set the state back to idle (0) if it was working (1) before
-                    let st = s.status.compare_and_swap(1, 0, Ordering::SeqCst);
+                    // set the state back to IDLE if it was WORKING before
+                    let st = s.status.compare_and_swap(WORKING, IDLE, SeqCst);
                     match st {
-                        1 => {
+                        WORKING => {
                             // status was updated from WORKING to IDLE
                             // are there still values in the queue?
                             if let Some(_) = s.queue.peek() {
                                 s.notify();
                             }
                         },
-                        2 => {
+                        STREAM_CLOSED => {
                             // status was NOT updated -> still exclusive access
                             // stream closed, sender doesn't exist anymore
                             // check for last new values
@@ -101,11 +104,12 @@ impl<T, S> SubscriberLink<T, S> where S: Subscriber<T> {
                     }
                 }
             },
-            1 => {}, // subscriber already working
+            WORKING => {}, // subscriber already working
             _ => unreachable!()
         }
     }
-
+    
+    // unsafe because there must be only one popper at any time
     unsafe fn pop_values(&mut self) {
         while let Some(value) = self.queue.pop() {
             self.subscriber.on_value(value);
