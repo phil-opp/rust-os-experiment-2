9e554adcc2f6c2f8f0265fb87cb02a04b2afcd3e
Philipp Oppermann <dev@phil-opp.com>
2015-05-07 15:16:19 +0200

    replace Box by Unique to avoid mem::forget

--- src/os/src/stream/spsc.rs
+++ src/os/src/stream/spsc.rs
@@ -1,31 +1,35 @@
-use std::mem;
+use std::boxed::into_raw;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::mpsc::spsc_queue::Queue;
+use std::ptr::Unique;
 use super::{Sender, Subscriber};
 
 pub struct SpscSender<T, S> {
-    subscriber: Box<SubscriberLink<T, S>>
+    subscriber: Unique<SubscriberLink<T, S>>
 }
 
 impl<T, S> SpscSender<T, S> where S: Subscriber<T> {
     pub fn new(subscriber: S) -> SpscSender<T, S> {
-        SpscSender {
-            subscriber: Box::new(SubscriberLink{
-                queue: unsafe{ Queue::new(0) },
-                status: AtomicUsize::new(0),
-                subscriber: subscriber,
-            }),
+        unsafe {
+            SpscSender {
+                subscriber: Unique::new(into_raw(Box::new(
+                    SubscriberLink{
+                        queue: Queue::new(0),   // unlimited cache
+                        status: AtomicUsize::new(0),
+                        subscriber: subscriber,
+                }))),
+            }            
         }
     }
 }
 
 impl<T, S> Sender<T> for SpscSender<T, S> where S: Subscriber<T> {
     fn send(&self, value: T) {
-        self.subscriber.new_value(value);
+        unsafe{ self.subscriber.get().new_value(value) };
     }
 
     fn close(self) {
-        self.subscriber.close();
+        unsafe{ self.subscriber.get().close() };
     }
 }
 
@@ -44,7 +48,7 @@ impl<T, S> SubscriberLink<T, S> where S: Subscriber<T> {
         self.notify();
     }
 
-    fn close(self: Box<Self>) {
+    fn close(&self) {
         match self.status.swap(2, Ordering::SeqCst) {
             1 => {},    // subscriber is currently working and will call
                         // the on_close function when done
@@ -52,8 +56,6 @@ impl<T, S> SubscriberLink<T, S> where S: Subscriber<T> {
                                 //pop all remaining values and call on_close
             _ => unreachable!()                
         }
-        // either way, we won't drop self
-        unsafe{ mem::forget(self) }
     }
 
     fn notify(&self) {
@@ -61,15 +63,14 @@ impl<T, S> SubscriberLink<T, S> where S: Subscriber<T> {
         match old_status {
             0 | 2 => { 
                 // if ==0 : subscriber was idle and is now working. We have marked
-                // it as working (1) have thus temporary ownership. We will set 
-                // the state back to idle (0) at the end and forget self to
-                // avoid starting the destructor.
+                // it as working (1) have thus exclusive access. We will set 
+                // the state back to idle (0) at the end.
                 // if ==2: Stream was closed and there won't be any new values. 
                 // But maybe there are still values in the queue. Pop them and
                 // then call on_close and drop self.
 
-                let mut s: Box<Self> = unsafe {
-                    Box::from_raw(self as *const _ as *mut _)};
+                // we have exclusive access
+                let s: &mut Self = unsafe{ &mut *(self as *const _ as *mut _) };
 
                 // TODO spawn task
                 {
@@ -78,15 +79,22 @@ impl<T, S> SubscriberLink<T, S> where S: Subscriber<T> {
                     let st = s.status.compare_and_swap(1, 0, Ordering::SeqCst);
                     match st {
                         1 => {
+                            // status was updated from WORKING to IDLE
                             // are there still values in the queue?
                             if let Some(_) = s.queue.peek() {
                                 s.notify();
                             }
-                            unsafe{ mem::forget(s) } // give up ownership
                         },
-                        2 => { // stream closed, sender doesn't exist anymore
-                            // check again for new values
+                        2 => {
+                            // status was NOT updated -> still exclusive access
+                            // stream closed, sender doesn't exist anymore
+                            // check for last new values
                             unsafe{ s.pop_values() };
+
+                            // take ownership
+                            let s: Box<Self> = unsafe {
+                                Box::from_raw(s as *mut _)};
+
                             s.subscriber.on_close();
                         }, 
                         _ => unreachable!()
