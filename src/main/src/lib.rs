e8ee91894e6ebd9ac8163dee44015a42aebc0fc0
Philipp Oppermann <dev@phil-opp.com>
2015-05-07 16:30:19 +0200

    init and test os and std functions

--- src/main/src/lib.rs
+++ src/main/src/lib.rs
@@ -1,8 +1,24 @@
 #![feature(lang_items)]
 
+use os::stream::*;
+
+extern crate os;
+
 pub mod unimplemented_functions;
 
 #[no_mangle]
-pub extern fn main() {
+pub extern fn main(multiboot: os::MultibootHeader) {
+    os::init(multiboot);
+
+    println!("Hello World!");
+
+    let s = SpscSender::on_value(|msg| println!("msg: {}", msg));
+
+    s.send("test1");
+    s.send("test2");
+    s.close();
+
+    print!("this is a ");
+    println!("test message...");
     loop{}
 }
