f93f291154e8a00f04fd73c558239eb1e922e3b7
Philipp Oppermann <dev@phil-opp.com>
2015-05-07 16:25:09 +0200

    add modules and panic function

--- src/os/src/lib.rs
+++ src/os/src/lib.rs
@@ -1,3 +1,24 @@
-#![feature(std_misc, alloc)]
+#![feature(std_misc, alloc, core, unique, lang_items)]
 
+pub use init::MultibootHeader;
+use std::fmt;
+
+#[macro_use]
+extern crate bitflags;
+
+pub mod io;
 pub mod stream;
+pub mod allocator;
+mod init;
+
+pub fn init(multiboot: init::MultibootHeader) {
+    init::init(multiboot);
+}
+
+#[lang = "panic_fmt"] 
+fn panic_fmt(msg: fmt::Arguments, file: &'static str, line: u32) -> ! {
+    use io::stdio::vga_buffer::*;
+    set_color(Color::White, Color::Red);
+    println!("\n\nPANIC");
+    loop {}
+}
