cbc7526e508f7799fd32013bec36f44c605641da
Philipp Oppermann <dev@phil-opp.com>
2015-06-10 14:37:45 +0200

    use tasks to print on interrupts

--- src/os/src/interrupts.rs
+++ src/os/src/interrupts.rs
@@ -1,12 +1,12 @@
 use enable_interrupts;
 use io::stdio;
 use std::fmt::Write;
+use global;
 
 #[no_mangle]
 pub extern "C" fn interrupt_handler(interrupt_number: isize, error_code: isize) {
-    let mut writer = unsafe{stdio::interrupt_screen_writer()};
     match interrupt_number {
-        32 => {}, // timer
+        32 => global::spawn(move || print!(".")), // timer
         _ => panic!("interrupt {} with error code 0x{:x}", interrupt_number, error_code),
     }
     unsafe{
@@ -22,10 +22,12 @@ pub extern fn pagefault_handler(address: usize, error_code: isize) {
 
 #[no_mangle]
 pub extern fn keyboard_handler(interrupt_number: isize, key_code: usize) {
+    unsafe{
+        send_eoi(interrupt_number);
+        enable_interrupts()
+    }
     assert!(interrupt_number == 33);
-    let mut writer = unsafe{stdio::interrupt_screen_writer()};
-    writer.write_fmt(format_args!("\nk {}", key_code));
-    unsafe{send_eoi(interrupt_number)};
+    global::spawn(move || print!(" [k {}] ", key_code));
 }
 
 unsafe fn send_eoi(interrupt_number: isize) {
