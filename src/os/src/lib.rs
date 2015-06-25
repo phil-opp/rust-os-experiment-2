d074f519d0c110066a0fe790e1665278f50a7386
Philipp Oppermann <dev@phil-opp.com>
2015-06-25 15:59:09 +0200

    allow new unstable features

--- src/os/src/lib.rs
+++ src/os/src/lib.rs
@@ -1,5 +1,7 @@
 #![feature(std_misc, alloc, core, unique, lang_items, asm, const_fn)]
 #![feature(optin_builtin_traits)]
+#![feature(core_intrinsics, box_raw, ptr_as_ref, fnbox)]
+#![feature(spsc_queue, mpsc_queue)]
 
 pub use init::MultibootHeader;
 use std::fmt;
