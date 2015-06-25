820f34613c83cc8627a333fc751d149592c623d6
Philipp Oppermann <dev@phil-opp.com>
2015-06-25 16:02:46 +0200

    new example output for new streams

--- src/main/src/lib.rs
+++ src/main/src/lib.rs
@@ -2,6 +2,7 @@
 
 use os::stream::*;
 use os::global;
+use std::fmt::Debug;
 
 extern crate os;
 
@@ -14,11 +15,21 @@ pub extern fn main(multiboot: os::MultibootHeader) {
     println!("Hello World!");
     unsafe{os::enable_interrupts()};
 
-    let s = SpscSender::on_value(|msg| println!("msg: {}", msg));
+    {
+        let (s, r) = stream();
 
-    s.send("test1");
-    s.send("test2");
-    s.close();
+        s.send(H(1));
+        s.send(H(2));
+        s.close();
+    }
+
+    let (tx, rx) = os::stream::stream();
+
+    tx.send("test1");
+    tx.send("test2");
+    rx.subscribe(Box::new(Dummy));
+    tx.send("test3");
+    tx.close();
 
     print!("this is a ");
     println!("test message...");
@@ -26,5 +37,25 @@ pub extern fn main(multiboot: os::MultibootHeader) {
         //for _ in 0..10000 {}
         //println!("running next task...");
         global::run_next_task()
+    }
+}
+
+struct Dummy;
+
+impl<T> Subscriber<T> for Dummy where T: Debug {
+    fn on_value(&mut self, value: T) {
+        println!("Dummy: {:?}", value);
+    }
+    fn on_close(self: Box<Self>) {
+        println!("Dummy: closed");
+    }
+}
+
+
+struct H(i32);
+
+impl Drop for H {
+    fn drop(&mut self) {
+        println!("dropping H {}", self.0);
     }
 }
